7ab79d4586d3cd07d63757ff9235a263f4b21072
nullsc <53406164+nullsc@users.noreply.github.com>
2023-10-08 20:01:35 +0100

    Create config.php

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$dbserver = "";
+$dbuser = "";
+$dbpass = ""; // 
+$dbname = ""; //
+
+$adminusername = "";
+$adminpassword = "";
+
+$apikey = ""; //not used yet
+$safemode = true;
+$logsPerPage = 20; // pagination
+
+?>
